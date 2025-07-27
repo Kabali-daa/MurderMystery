fc428c6d970ac8fe7f19bd704faa5c4841f6cb63
Kabali-daa <karthikeyanthunder@gmail.com>
2025-07-27 17:45:56 +0530

    Update App.js

--- murder-mystery-game/src/App.js
+++ murder-mystery-game/src/App.js
@@ -6,13 +6,13 @@ import { getFirestore, doc, getDoc, setDoc, updateDoc, onSnapshot, collection, w
 // Firebase Configuration and Initialization
 // IMPORTANT: Replace this with your own Firebase config object!
 const firebaseConfig = {
-  apiKey: "AIzaSyDBSnP8kcNiXs9j490Xs8w2Vos4ZklY9FE",
-  authDomain: "murder-mystery-kabali.firebaseapp.com",
-  projectId: "murder-mystery-kabali",
-  storageBucket: "murder-mystery-kabali.firebasestorage.app",
-  messagingSenderId: "412262393697",
-  appId: "1:412262393697:web:19133180557995c6480720",
-  measurementId: "G-PZTEMJN0LB"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
