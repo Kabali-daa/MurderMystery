d7321322b899f0cb3c55de60a37140a9b6dd5e17
Kabali-daa <karthikeyanthunder@gmail.com>
2025-08-04 09:57:51 +0530

    Create contexts.js

---
+++ murder-mystery-game/src/contexts.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+// Context for Authentication and core app state
+export const AuthContext = createContext(null);
+
+// Context for Game-specific details
+export const GameContext = createContext(null);
